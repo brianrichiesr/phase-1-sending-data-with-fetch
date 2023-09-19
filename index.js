c4d83c877b6f24d9b85612e6b4eb34a458de3618
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-18 23:36:13 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+const body = document.querySelector("body");
+
+const submitData = (name, email) => {
+    const userObj = {
+        "name": name,
+        "email": email
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userObj)
+    })
+    .then(response => response.json())
+    .then(data => {
+        body.append(data.id);
+    })
+    .catch(err => body.append(err))
+}
